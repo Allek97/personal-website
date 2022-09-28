08990a2d6cc81d7717081cbbaf5d6ed1de1960ab
Allek97 <ilyas.jsk@hotmail.ca>
2022-09-28 18:31:12 -0400

    Removed the 3d globe cause it slows the website tremendously

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,14 +3,10 @@ import React, { useRef } from "react";
 import { motion } from "framer-motion";
 import { Element } from "react-scroll";
 
-import { useGlobe, useGlobeUpdate } from "../../context/GlobeContext";
 import { useSiteDataImages } from "../../hooks/useSiteDataImages";
-import { useLoading } from "../../context/LoadingContext";
 
 import {
   ExtraPlanetImage,
-  GlobeCanvas,
-  GlobeContainer,
   GlobeTooltip,
   Header,
   HeroArticle,
@@ -30,15 +26,7 @@ const textSecondary =
 const Hero = () => {
   const homeRef = useRef();
 
-  const isGlobe = useGlobe();
-  //   const isGlobe = true;
-  const setIsGlobe = useGlobeUpdate();
-
-  const isLoaded = useLoading();
-  //   const isLoaded = true;
-
-  const { beyondGlobeImage, myGlobeImage, beyondImage, rocketImage } =
-    useSiteDataImages();
+  const { myGlobeImage, beyondImage, rocketImage } = useSiteDataImages();
 
   const toolTipMotion = {
     rest: { opacity: 0, y: "10px", scale: 0.95 },
@@ -58,7 +46,7 @@ const Hero = () => {
     initial: {
       scale: 0,
     },
-    animate: isLoaded && {
+    animate: {
       scale: 1,
     },
     transition: {
@@ -89,110 +77,65 @@ const Hero = () => {
     <Element name="homeSection">
       <Header id="home" ref={homeRef}>
         <HeroArticle>
-          {isLoaded && (
-            <div style={{ width: "100%" }}>
-              <AnimateText
-                isAnimate={isLoaded}
-                text={textHeader}
-                type="heading1"
-              />
-              <AnimateText
-                isAnimate={isLoaded}
-                text={textSecondary}
-                type="paragraph"
-                staggerValue={0.01}
-                version="fadeIn"
-              />
-              <div>
-                <HeroBtn
-                  aria-label="globe theme toggle"
-                  initial="rest"
-                  whileHover="hover"
-                  whileTap="hover"
+          <div style={{ width: "100%" }}>
+            <AnimateText isAnimate text={textHeader} type="heading1" />
+            <AnimateText
+              isAnimate
+              text={textSecondary}
+              type="paragraph"
+              staggerValue={0.01}
+              version="fadeIn"
+            />
+            <div>
+              <HeroBtn
+                aria-label="globe theme toggle"
+                initial="rest"
+                whileHover="hover"
+                whileTap="hover"
+              >
+                <motion.div
+                  whileHover={globeMotion3D}
+                  whileTap={globeMotion3D}
+                  style={{ width: "100%", height: "auto" }}
                 >
-                  {isGlobe ? (
-                    <>
-                      <motion.div
-                        whileHover={globeMotion2D}
-                        whileTap={globeMotion2D}
-                        style={{ width: "100%", height: "auto" }}
-                        onClick={() => {
-                          setIsGlobe(false);
-                        }}
-                      >
-                        <motion.div
-                          {...globeMotion}
-                          style={{ width: "100%", height: "auto" }}
-                        >
-                          <PlanetImage
-                            image={beyondGlobeImage}
-                            alt="small-planet"
-                          />
-                        </motion.div>
-                      </motion.div>
-                      <GlobeTooltip variants={toolTipMotion}>
-                        Activate 2D Image
-                      </GlobeTooltip>
-                    </>
-                  ) : (
-                    <>
-                      <motion.div
-                        whileHover={globeMotion3D}
-                        whileTap={globeMotion3D}
-                        style={{ width: "100%", height: "auto" }}
-                      >
-                        <motion.div
-                          {...globeMotion}
-                          style={{ width: "100%", height: "auto" }}
-                        >
-                          <PlanetImage
-                            image={myGlobeImage}
-                            alt="small-globe"
-                            onClick={() => {
-                              window.location.reload();
-                            }}
-                            isgray="true"
-                          />
-                        </motion.div>
-                      </motion.div>
+                  <motion.div
+                    {...globeMotion}
+                    style={{ width: "100%", height: "auto" }}
+                  >
+                    <PlanetImage
+                      image={myGlobeImage}
+                      alt="small-globe"
+                      onClick={() => {
+                        window.location.reload();
+                      }}
+                      isgray="true"
+                    />
+                  </motion.div>
+                </motion.div>
 
-                      <GlobeTooltip variants={toolTipMotion}>
-                        Activate 3D Globe
-                      </GlobeTooltip>
-                    </>
-                  )}
-                </HeroBtn>
-              </div>
+                <GlobeTooltip variants={toolTipMotion}>
+                  Activate 3D Globe
+                </GlobeTooltip>
+              </HeroBtn>
             </div>
-          )}
-          {isGlobe ? (
-            <GlobeContainer>
-              <GlobeCanvas {...globeMotion} id="globe_canvas" />
-            </GlobeContainer>
-          ) : (
-            <motion.div {...globeMotion}>
-              <ExtraPlanetImage
-                image={beyondImage}
-                alt="planet with spaceship"
-              />
-            </motion.div>
-          )}
-        </HeroArticle>
-        {isLoaded && (
-          <>
-            <UFOContainer
-              initial={{ y: "5rem", scale: 0.9, opacity: 0 }}
-              animate={isLoaded && { y: "0rem", scale: 1, opacity: 1 }}
-              transition={{
-                delay: 0.3,
-              }}
-            >
-              <UFOImage image={rocketImage} alt="Ufo" />
-            </UFOContainer>
+          </div>
 
-            <Stand />
-          </>
-        )}
+          <motion.div {...globeMotion}>
+            <ExtraPlanetImage image={beyondImage} alt="planet with spaceship" />
+          </motion.div>
+        </HeroArticle>
+
+        <UFOContainer
+          initial={{ y: "5rem", scale: 0.9, opacity: 0 }}
+          animate={{ y: "0rem", scale: 1, opacity: 1 }}
+          transition={{
+            delay: 0.3,
+          }}
+        >
+          <UFOImage image={rocketImage} alt="Ufo" />
+        </UFOContainer>
+
+        <Stand />
       </Header>
     </Element>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,8 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 import React, { useEffect, useRef } from "react";
-import loadable from "@loadable/component";
 import { Element } from "react-scroll";
 import { useInView } from "react-intersection-observer";
 
-import Loading from "../components/loading/Loading";
 import NavBar from "../components/navBar/NavBar";
 import Projects from "../components/projects/Projects";
 import Contact from "../components/contact/Contact";
@@ -18,16 +16,11 @@ import {
 
 import { ContactSection } from "../styles/indexStyles/ContactSectionStyle";
 
-import { useLoading } from "../context/LoadingContext";
-
 import { useScrollEffect } from "../hooks/useScrollEffect";
 import Hero from "../components/hero/Hero";
 import Coding from "../components/coding/Coding";
 import About from "../components/about/About";
 import AnimateText from "../components/utils/animations/AnimateText";
-import { useGlobe } from "../context/GlobeContext";
-
-const HomeGlobe = loadable(() => import("../components/HomeGlobe"));
 
 // import { HomeGlobe } from "../components/HomeGlobe";
 
@@ -35,12 +28,7 @@ export default function Index() {
   // Gatsby Link component retaining scroll position and not resetting to top
   useEffect(() => window.scrollTo(0, 0), []);
 
-  const isGlobe = useGlobe();
-
-  const loadTime = 20000;
-  const isLoaded = useLoading();
-  //   const isLoaded = true;
-  useScrollEffect(isLoaded);
+  useScrollEffect(true);
 
   ////////////////////////////////
   // NOTE: SCROLL ANIMATIONS
@@ -61,41 +49,39 @@ export default function Index() {
   return (
     <>
       <Seo title="Home" />
-      {isGlobe && <HomeGlobe />}
-      {!isLoaded && <Loading timeLoad={loadTime} />}
-      {isLoaded && <ContactDetails />}
-      {isLoaded && <NavBar navColor="white" />}
+
+      {/* {!isLoaded && <Loading timeLoad={loadTime} />} */}
+      <ContactDetails />
+      <NavBar navColor="white" />
       <div>
         <Hero />
-        {isLoaded && (
-          <main>
-            <Coding />
-            <About />
+        <main>
+          <Coding />
+          <About />
 
-            <Element name="projectSection">
-              <ProjectSection id="project" ref={projectRef}>
-                <ProjectContainer>
-                  <AnimateText
-                    text="Recent Projects"
-                    type="heading1"
-                    isAnimate={isProjectInView}
-                    version="slideUp"
-                    staggerValue={0.025}
-                    letterDuration={0.5}
-                    refAnimation={projectHeaderRef}
-                  />
-                  <Projects />
-                </ProjectContainer>
-              </ProjectSection>
-            </Element>
+          <Element name="projectSection">
+            <ProjectSection id="project" ref={projectRef}>
+              <ProjectContainer>
+                <AnimateText
+                  text="Recent Projects"
+                  type="heading1"
+                  isAnimate={isProjectInView}
+                  version="slideUp"
+                  staggerValue={0.025}
+                  letterDuration={0.5}
+                  refAnimation={projectHeaderRef}
+                />
+                <Projects />
+              </ProjectContainer>
+            </ProjectSection>
+          </Element>
 
-            <Element name="contactSection">
-              <ContactSection id="contact" ref={contactRef}>
-                <Contact />
-              </ContactSection>
-            </Element>
-          </main>
-        )}
+          <Element name="contactSection">
+            <ContactSection id="contact" ref={contactRef}>
+              <Contact />
+            </ContactSection>
+          </Element>
+        </main>
       </div>
     </>
   );
